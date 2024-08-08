8d31030bb79937f958f1259d256c442de40fa9fe
Viacheslav Poliakov <159778173+viacheslavpoliakov@users.noreply.github.com>
2024-08-08 15:45:39 +0300

    feat(specification-validation) implement Invalid Field Tag validation (#48)
    * feat(specification-validation) implement Invalid Field Tag validation
    
    - Add FieldTagRuleValidator
    
    Closes: MRSPECS-44

---
+++ mod-record-specifications-validator/src/main/java/org/folio/rspec/validation/validator/marc/impl/FieldTagRuleValidator.java
@@ -0,0 +1,61 @@
+package org.folio.rspec.validation.validator.marc.impl;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import org.folio.rspec.domain.dto.DefinitionType;
+import org.folio.rspec.domain.dto.SeverityType;
+import org.folio.rspec.domain.dto.SpecificationDto;
+import org.folio.rspec.domain.dto.ValidationError;
+import org.folio.rspec.i18n.TranslationProvider;
+import org.folio.rspec.validation.validator.SpecificationRuleCode;
+import org.folio.rspec.validation.validator.SpecificationRuleValidator;
+import org.folio.rspec.validation.validator.marc.model.MarcField;
+import org.folio.rspec.validation.validator.marc.model.MarcRuleCode;
+import org.folio.rspec.validation.validator.marc.utils.TagsMatcher;
+
+class FieldTagRuleValidator
+  implements SpecificationRuleValidator<Map<String, List<MarcField>>, SpecificationDto> {
+
+  private final TranslationProvider translationProvider;
+
+  FieldTagRuleValidator(TranslationProvider translationProvider) {
+    this.translationProvider = translationProvider;
+  }
+
+  @Override
+  public List<ValidationError> validate(Map<String, List<MarcField>> fields, SpecificationDto specification) {
+    return fields.values().stream()
+      .flatMap(Collection::stream)
+      .filter(field -> !TagsMatcher.matchesValidTag(field.tag()))
+      .map(field -> buildError(field, specification))
+      .toList();
+  }
+
+  @Override
+  public SpecificationRuleCode supportedRule() {
+    return MarcRuleCode.INVALID_FIELD_TAG;
+  }
+
+  @Override
+  public DefinitionType definitionType() {
+    return DefinitionType.FIELD;
+  }
+
+  @Override
+  public SeverityType severity() {
+    return SeverityType.ERROR;
+  }
+
+  private ValidationError buildError(MarcField marcField, SpecificationDto specificationDto) {
+    var message = translationProvider.format(ruleCode());
+    return ValidationError.builder()
+      .path(marcField.reference().toString())
+      .definitionType(definitionType())
+      .definitionId(specificationDto.getId())
+      .severity(severity())
+      .ruleCode(ruleCode())
+      .message(message)
+      .build();
+  }
+}
--- mod-record-specifications-validator/src/main/java/org/folio/rspec/validation/validator/marc/utils/TagsMatcher.java
+++ mod-record-specifications-validator/src/main/java/org/folio/rspec/validation/validator/marc/utils/TagsMatcher.java
@@ -3,10 +3,15 @@ package org.folio.rspec.validation.validator.marc.utils;
 import java.util.regex.Pattern;
 
 public final class TagsMatcher {
+  public static final Pattern TAG_PATTERN = Pattern.compile("^\\d{3}$");
   private static final Pattern TAG_1XX_PATTERN = Pattern.compile("^1\\d{2}$");
 
   private TagsMatcher() {
     throw new IllegalStateException("Utility class");
+  }
+
+  public static boolean matchesValidTag(String tag) {
+    return tag != null && TAG_PATTERN.matcher(tag).matches();
   }
 
   public static boolean matches1xx(String tag) {
