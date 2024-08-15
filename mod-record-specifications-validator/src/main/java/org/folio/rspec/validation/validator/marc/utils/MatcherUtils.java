524535888dac723476b661e96babcb7fd18dcc8a
Viacheslav_Poliakov <Viacheslav_Poliakov@epam.com>
2024-08-15 09:38:04 +0300

    feat(specification-validation) implement Invalid Indicator validation
    - Update utility class
    
    Closes: MRSPECS-45

--- mod-record-specifications-validator/src/main/java/org/folio/rspec/validation/validator/marc/utils/MatcherUtils.java
+++ mod-record-specifications-validator/src/main/java/org/folio/rspec/validation/validator/marc/utils/MatcherUtils.java
@@ -1,14 +1,12 @@
 package org.folio.rspec.validation.validator.marc.utils;
 
 import java.util.regex.Pattern;
+import lombok.experimental.UtilityClass;
 
+@UtilityClass
 public final class MatcherUtils {
   public static final Pattern TAG_PATTERN = Pattern.compile("^\\d{3}$");
   private static final Pattern TAG_1XX_PATTERN = Pattern.compile("^1\\d{2}$");
-
-  private MatcherUtils() {
-    throw new IllegalStateException("Utility class");
-  }
 
   public static boolean matchesValidTag(String tag) {
     return tag != null && TAG_PATTERN.matcher(tag).matches();
