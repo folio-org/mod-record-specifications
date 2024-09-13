af54272dc825bb15f3881288cb248e9186edbb65
Mukhiddin Yusupov <mukhiddin_yusupov@epam.com>
2024-09-13 11:17:59 +0500

    feat(specification-sync): sync specifications on tenant creation
    Closes: MRSPECS-53

--- mod-record-specifications-server/src/main/java/org/folio/rspec/service/tenant/ExtendedTenantService.java
+++ mod-record-specifications-server/src/main/java/org/folio/rspec/service/tenant/ExtendedTenantService.java
@@ -1,6 +1,9 @@
 package org.folio.rspec.service.tenant;
 
 import lombok.extern.log4j.Log4j2;
+import org.folio.rspec.domain.dto.IncludeParam;
+import org.folio.rspec.domain.dto.SpecificationDto;
+import org.folio.rspec.service.SpecificationService;
 import org.folio.spring.FolioExecutionContext;
 import org.folio.spring.liquibase.FolioSpringLiquibase;
 import org.folio.spring.service.TenantService;
@@ -17,15 +20,17 @@ public class ExtendedTenantService extends TenantService {
 
   private final FolioExecutionContext folioExecutionContext;
   private final KafkaAdminService kafkaAdminService;
+  private final SpecificationService specificationService;
 
   public ExtendedTenantService(JdbcTemplate jdbcTemplate,
-                               FolioExecutionContext context,
                                FolioSpringLiquibase folioSpringLiquibase,
                                FolioExecutionContext folioExecutionContext,
-                               KafkaAdminService kafkaAdminService) {
-    super(jdbcTemplate, context, folioSpringLiquibase);
+                               KafkaAdminService kafkaAdminService,
+                               SpecificationService specificationService) {
+    super(jdbcTemplate, folioExecutionContext, folioSpringLiquibase);
     this.folioExecutionContext = folioExecutionContext;
     this.kafkaAdminService = kafkaAdminService;
+    this.specificationService = specificationService;
   }
 
   @Override
@@ -38,5 +43,21 @@ public class ExtendedTenantService extends TenantService {
   protected void afterTenantDeletion(TenantAttributes tenantAttributes) {
     var tenantId = context.getTenantId();
     kafkaAdminService.deleteTopics(tenantId);
+  }
+
+  @Override
+  public synchronized void createOrUpdateTenant(TenantAttributes tenantAttributes) {
+    if (Boolean.FALSE.equals(tenantExists())) {
+      super.createOrUpdateTenant(tenantAttributes);
+      var specificationIds = specificationService.findSpecifications(null, null, IncludeParam.NONE, 100, 0)
+        .getSpecifications().stream()
+        .map(SpecificationDto::getId)
+        .toList();
+
+      log.info("About to start record specifications syncing");
+      for (var id : specificationIds) {
+        specificationService.sync(id);
+      }
+    }
   }
 }
