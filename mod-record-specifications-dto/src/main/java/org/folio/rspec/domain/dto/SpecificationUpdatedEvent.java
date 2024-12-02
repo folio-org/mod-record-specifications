1c375ddf617ceaa64fd686fe0a5fe38886235a96
SvitlanaKovalova1 <Svitlana_Kovalova1@epam.com>
2024-12-02 11:34:08 +0200

    test(build-dependants) build dependants workflow testing

--- mod-record-specifications-dto/src/main/java/org/folio/rspec/domain/dto/SpecificationUpdatedEvent.java
+++ mod-record-specifications-dto/src/main/java/org/folio/rspec/domain/dto/SpecificationUpdatedEvent.java
@@ -3,10 +3,7 @@ package org.folio.rspec.domain.dto;
 import java.util.UUID;
 
 public record SpecificationUpdatedEvent(UUID specificationId,
-                                        String tenantId,
-                                        Family family,
-                                        FamilyProfile profile,
-                                        UpdateExtent updateExtent) {
+                                        String tenantId) {
 
   public enum UpdateExtent {
     FULL, PARTIAL
